f38bcab95b8aebdc9bcd89412c3d2b27b6e9db0b
Nate <nateaba@gmail.com>
2023-06-02 21:25:44 -0700

    Add left section code to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-import DownSVG from "./components/DownSVG.vue";
+import Left from "./components/Left.vue";
 </script>
 
 <template>
-  <div id="left">
-    <div class="relative mt-8 h-screen">
-      <h1 class="text-2xl">Nate Abaria</h1>
-
-      <p>Web Developer and Designer</p>
-
-      <p class="mt-[10rem]">
-        Specializing in simplicity <br />
-        and functionality
-      </p>
-    </div>
-    <DownSVG />
-  </div>
+  <Left />
 
   <div id="right"></div>
 </template>
---
+++ src/components/Left.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="left">
+    <div class="relative mt-14 h-screen">
+      <h1 class="text-[1.75rem]">Nate Abaria</h1>
+
+      <p>Web Developer and Designer</p>
+
+      <p class="mt-[10rem]">
+        Specializing in simplicity <br />
+        and functionality
+      </p>
+    </div>
+    <DownSVG />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DownSVG from "./DownSVG.vue";
+</script>
