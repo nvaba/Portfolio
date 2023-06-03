e0eeef4e762532ec28b8d5bc77668c1ba9b30bed
Nate <nateaba@gmail.com>
2023-06-02 21:18:22 -0700

    Add mobile hero section

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DownSVG from "./components/DownSVG.vue";
+</script>
 
 <template>
-  <h1 class="text-3xl">Nate Abaria</h1>
-  <p>Nate Abaria</p>
+  <div id="left">
+    <div class="relative mt-8 h-screen">
+      <h1 class="text-2xl">Nate Abaria</h1>
+
+      <p>Web Developer and Designer</p>
+
+      <p class="mt-[10rem]">
+        Specializing in simplicity <br />
+        and functionality
+      </p>
+    </div>
+    <DownSVG />
+  </div>
+
+  <div id="right"></div>
 </template>
 
 <style scoped></style>
---
+++ src/components/DownSVG.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    class="hoverUpDown absolute left-1/2 top-[80%] -translate-x-1/2 transform"
+  >
+    <path
+      fill="none"
+      stroke="#dfdfdf"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="4"
+      d="M36 12L24 24L12 12m24 12L24 36L12 24"
+    />
+  </svg>
+</template>
+
+<style>
+@keyframes hoverUpDown {
+  0% {
+    transform: translateX(-50%) translateY(0);
+  }
+  50% {
+    transform: translateX(-50%) translateY(-20px);
+  }
+  100% {
+    transform: translateX(-50%) translateY(0);
+  }
+}
+
+.hoverUpDown {
+  animation: hoverUpDown 2s ease-in-out infinite;
+}
+</style>
