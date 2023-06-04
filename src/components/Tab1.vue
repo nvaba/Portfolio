55d7487fa2edb6a55b1794fe36db9b0a0c328af1
Nate <nateaba@gmail.com>
2023-06-04 16:23:38 -0700

    Add types to tab1 component

--- src/components/Tab1.vue
+++ src/components/Tab1.vue
@@ -1,3 +1,34 @@
 <template>
-  <p>test1</p>
+  <h4 class="mt-4">{{ item.acf.tab_1_repeater[0].tab_1_heading_1 }}</h4>
 </template>
+
+<script setup lang="ts">
+interface AccordionContentProps {
+  item: {
+    id: number;
+    title: {
+      rendered: string;
+    };
+    acf: {
+      overview_title: string;
+      overview_text: string;
+      live_website_url: string;
+      repo_url: string;
+      tab_title_1: string;
+      tab_title_2: string;
+      tab_title_3: string;
+      tab_1_repeater: {
+        preview_gif: string;
+        tab_1_heading_1: string;
+        tab_1_text_area: string;
+        // Add more properties as needed
+      }[];
+    };
+    // Add more properties as needed
+  };
+}
+
+// Define the props
+const props = defineProps<AccordionContentProps>();
+console.log(props.item.acf.repo_url);
+</script>
