1692ba85e5072abcfc8e0c190ec69d6672d34ed2
Nate <nateaba@gmail.com>
2023-06-05 17:17:14 -0700

    Import about component to app

--- src/components/Right.vue
+++ src/components/Right.vue
@@ -2,10 +2,12 @@
   <div id="right" class="lg:w-1/2 lg:border-l lg:border-text lg:pl-32">
     <FeaturedProjects />
     <VisualArts />
+    <About />
   </div>
 </template>
 
 <script setup lang="ts">
 import FeaturedProjects from "./FeaturedProjects.vue";
 import VisualArts from "./VisualArts.vue";
+import About from "./About.vue";
 </script>
