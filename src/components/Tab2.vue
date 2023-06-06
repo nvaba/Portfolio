6a572cc59af589d83f4ea31ddd3582a3d0eec246
Nate <nateaba@gmail.com>
2023-06-06 15:04:33 -0700

    Add dynamic code to tab 2 and 3

--- src/components/Tab2.vue
+++ src/components/Tab2.vue
@@ -1,3 +1,49 @@
 <template>
-  <p>test2</p>
+  <div>
+    <article
+      v-for="section in item.acf.tab_2_repeater_content"
+      :key="section.tab_2_content_title"
+    >
+      <h4 class="mt-8">{{ section.tab_2_content_title }}</h4>
+      <div class="imagediv mt-4">
+        <img
+          :src="section.tab_2_content_preview.url"
+          :alt="section.tab_2_content_preview.alt"
+        />
+      </div>
+      <p class="mt-5">
+        {{ section.tab_2_text_area }}
+      </p>
+    </article>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { defineProps, onMounted } from "vue";
+
+interface AccordionContentProps {
+  item: {
+    id: number;
+    acf: {
+      tab_2_repeater_content: {
+        tab_2_content_title: string;
+        tab_2_content_preview: {
+          url: string;
+          alt: string;
+        };
+        tab_2_text_area: string;
+      }[];
+      // Add more properties as needed
+    };
+    // Add more properties as needed
+  };
+}
+
+// Define the props
+const props = defineProps<AccordionContentProps>();
+
+// Log the contents of tab_2_repeater_content
+onMounted(() => {
+  console.log(props.item.acf.tab_2_repeater_content);
+});
+</script>
--- src/components/Tab3.vue
+++ src/components/Tab3.vue
@@ -1,3 +1,46 @@
 <template>
-  <p>test3</p>
+  <div>
+    <article
+      v-for="section in item.acf.tab_3_repeater_content"
+      :key="section.tab_3_content_title"
+    >
+      <h4 class="mt-8">{{ section.tab_3_content_title }}</h4>
+      <div class="imagediv mt-4">
+        <img
+          :src="section.tab_3_content_preview.url"
+          :alt="section.tab_3_content_preview.alt"
+        />
+      </div>
+      <p class="mt-5">
+        {{ section.tab_3_text_area }}
+      </p>
+    </article>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface AccordionContentProps {
+  item: {
+    id: number;
+    acf: {
+      tab_3_repeater_content: {
+        tab_3_content_title: string;
+        tab_3_content_preview: {
+          url: string;
+          alt: string;
+        };
+        tab_3_text_area: string;
+      }[];
+      // Add more properties as needed
+    };
+    // Add more properties as needed
+  };
+}
+
+// Define the props
+defineProps<AccordionContentProps>();
+
+// Log the contents of tab_2_repeater_content
+</script>
