a6f81b6084a3e2366da17b6b95accf088ae3428a
Nate <nateaba@gmail.com>
2023-06-08 14:07:33 -0700

    Move languages section to seperate component

---
+++ src/components/Languages.vue
@@ -0,0 +1,7 @@
+<template>
+  <article class="mt-7">
+    <h3>Developer Stack</h3>
+
+    <p>test</p>
+  </article>
+</template>
--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -1,16 +1,10 @@
 <template>
   <section>
     <h2 id="stack" class="mt-[5rem]">Things I Use</h2>
-    <article class="mt-7">
-      <h3>Developer Stack</h3>
-
-      <p>test</p>
-    </article>
-
-    <article class="mt-7">
-      <h3>Software and Programs</h3>
-
-      <p>test</p>
-    </article>
+    <Languages />
   </section>
 </template>
+
+<script setup lang="ts">
+import Languages from "./Languages.vue";
+</script>
