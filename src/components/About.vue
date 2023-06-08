c06eb27fdf3a9bb2333aa85e2d6dda9e88741c6c
Nate <nateaba@gmail.com>
2023-06-07 19:01:27 -0700

    Add API fetch in About component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,44 @@
 <template>
-  <h2 id="about" class="mt-[7rem]">About</h2>
+  <section>
+    <h2 id="about" class="mt-[7rem]">About</h2>
+    <div
+      class="about-list mt-2"
+      v-if="aboutContent.length"
+      v-html="aboutContent[0].acf.about_list"
+    ></div>
+  </section>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+interface AboutItem {
+  id: number;
+  acf: {
+    about_list: string;
+  };
+}
+
+const aboutContent = ref<AboutItem[]>([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(
+      "https://nateabaria.ca/naportfolio/wp-json/wp/v2/about"
+    );
+    aboutContent.value = response.data;
+
+    // log the data here
+    console.log(aboutContent.value[0].acf.about_list);
+  } catch (error) {
+    console.error("Failed to fetch about items:", error);
+  }
+});
+</script>
+
+<style>
+.about-list li {
+  margin-bottom: 0.25rem;
+}
+</style>
