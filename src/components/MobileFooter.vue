497fb6074376f3c39b8ae6f39a1cdb0eac7a4c3c
Nate <nateaba@gmail.com>
2023-06-09 20:41:49 -0700

    Add copy email function in mobile footer

--- src/components/MobileFooter.vue
+++ src/components/MobileFooter.vue
@@ -4,9 +4,10 @@
       <h4 class="text-center">Let's Connect!</h4>
       <div class="mx-auto mt-2 text-center">
         <button
-          class="rounded-xl bg-panel px-8 py-2 text-[0.8rem] text-headingtext transition-all duration-300 ease-in-out hover:bg-accent hover:text-panel active:bg-accent active:text-panel"
+          @click="copyEmail"
+          class="w-[12.5rem] rounded-xl bg-panel px-8 py-2 text-[0.8rem] text-headingtext transition-all duration-300 ease-in-out hover:translate-y-[-0.125rem] hover:bg-accent hover:text-panel active:bg-accent active:text-panel"
         >
-          Copy Email
+          {{ buttonText }}
         </button>
       </div>
     </section>
@@ -18,4 +19,25 @@
   </footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+let buttonText = ref("Copy Email");
+
+const copyEmail = () => {
+  navigator.clipboard
+    .writeText("nateaba@gmail.com")
+    .then(() => {
+      // Show a message saying that the email was copied
+      buttonText.value = "Email Copied!";
+      // Change back to original text after 3 seconds
+      setTimeout(() => {
+        buttonText.value = "Copy Email";
+      }, 2000);
+    })
+    .catch((err) => {
+      // Could not copy text: display a warning
+      console.warn("Could not copy email", err);
+    });
+};
+</script>
