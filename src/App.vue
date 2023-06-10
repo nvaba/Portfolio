47e24c0e153efd6bf0c3108663a66b4c0953f8d8
Nate <nateaba@gmail.com>
2023-06-09 20:12:47 -0700

    Add draft mobile nav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Left from "./components/Left.vue";
 import Right from "./components/Right.vue";
+// import MobileNav from "./components/MobileNav.vue";
 </script>
 
 <template>
   <div id="site-wrapper">
     <Left />
     <Right />
+    <!-- <MobileNav /> -->
   </div>
 </template>
 
---
+++ src/components/MobileNav.vue
@@ -0,0 +1,5 @@
+<template>
+  <div
+    class="fixed bottom-4 left-0 right-0 mx-auto h-[20px] w-[90%] max-w-[350px] bg-navigation lg:hidden"
+  ></div>
+</template>
