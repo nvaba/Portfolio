eaebc97f0222536d8de83fbaa0c05f9af59f835a
Nate <nateaba@gmail.com>
2023-06-10 22:16:23 -0700

    Add semantic header tag

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -4,14 +4,14 @@
     class="fade-in-slide-up h-screen w-full lg:fixed lg:w-1/2 lg:px-16 xl:px-32"
   >
     <div class="relative mt-20 h-full justify-between lg:flex lg:flex-col">
-      <div>
+      <header>
         <h1>Nate Abaria</h1>
         <p>Web Developer and Designer</p>
         <p class="mt-[14rem] lg:mt-16 lg:text-[1rem]">
           Specializing in simplicity <br />
           and functionality
         </p>
-      </div>
+      </header>
       <DesktopNav />
       <DownSVG />
       <DesktopFooter />
