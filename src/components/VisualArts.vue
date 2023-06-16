2a65256adf5ad420ed92079d22c757a40608af02
Nate <nateaba@gmail.com>
2023-06-15 19:14:49 -0700

    Add view gallery link to Visual Arts component

--- src/components/VisualArts.vue
+++ src/components/VisualArts.vue
@@ -1,6 +1,23 @@
 <template>
   <section class="fade-in-slide-up mt-[5rem]">
     <h2 id="visual-arts">Visual Arts</h2>
-    <p class="mt-2 text-accent">Gallery Coming Soon</p>
+    <p>A collection of my work outside of web development.</p>
+    <a
+      target="_blank"
+      class="group mt-3 inline-block text-accent active:text-headingtext lg:transition-colors lg:duration-100 lg:ease-in-out lg:hover:text-headingtext"
+      href="https://nateabaria.ca/gallery/"
+    >
+      <span class="flex items-center gap-1">
+        View Gallery
+        <SVGRight
+          class="lg:transition-transform lg:duration-200 lg:group-hover:translate-x-[0.25rem]"
+        />
+        <!-- Translate the SVGRight component on hover -->
+      </span>
+    </a>
   </section>
 </template>
+
+<script setup lang="ts">
+import SVGRight from "./SVGRight.vue";
+</script>
