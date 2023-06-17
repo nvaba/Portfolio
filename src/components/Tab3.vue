b1e6170746ce1fa6b4193fde6d4c5533cb40c003
Nate <nateaba@gmail.com>
2023-06-16 22:05:00 -0700

    Remove image section in tab3

--- src/components/Tab3.vue
+++ src/components/Tab3.vue
@@ -15,13 +15,7 @@
     >
       <article>
         <h4 class="mt-8">{{ section.tab_3_content_title }}</h4>
-        <div class="imagediv mt-4">
-          <img
-            :src="section.tab_3_content_preview.url"
-            :alt="section.tab_3_content_preview.alt"
-          />
-        </div>
-        <p class="mt-5 text-[0.95rem] text-text">
+        <p class="mt-2 text-[0.95rem] text-text">
           {{ section.tab_3_text_area }}
         </p>
       </article>
@@ -55,10 +49,6 @@ interface AccordionContentProps {
     acf: {
       tab_3_repeater_content: {
         tab_3_content_title: string;
-        tab_3_content_preview: {
-          url: string;
-          alt: string;
-        };
         tab_3_text_area: string;
       }[];
       // Add more properties as needed
