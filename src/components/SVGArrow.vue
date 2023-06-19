bc560f5c78ef56e68eeba955771f9c4378e745c7
Nate <nateaba@gmail.com>
2023-06-18 19:28:22 -0700

    Edit svg fill color and stroke width

--- src/components/SVGArrow.vue
+++ src/components/SVGArrow.vue
@@ -1,15 +1,16 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="15"
-    height="15"
+    width="18"
+    height="18"
     viewBox="0 0 15 15"
     class="h-7 transform transition-transform duration-300"
     :class="{ 'rotate-180': active }"
   >
     <path
       fill="none"
-      stroke="#f8f8f8"
+      stroke="#7B8DA3"
+      stroke-width="1.5"
       stroke-linecap="square"
       d="m14 5l-6.5 7L1 5"
     />
