8998db08366ee26347dc42a067ae144a0f47b310
Nate <nateaba@gmail.com>
2023-06-18 19:21:27 -0700

    Edit heading margin

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -3,11 +3,13 @@
     id="left"
     class="fade-in-slide-up h-screen w-full lg:fixed lg:w-1/2 lg:px-16 xl:px-32"
   >
-    <div class="relative mt-20 h-full justify-between lg:flex lg:flex-col">
+    <div
+      class="relative mt-20 h-full justify-between lg:flex lg:flex-col lg:gap-6"
+    >
       <header>
         <h1>Nate Abaria</h1>
         <p>Web Developer and Designer</p>
-        <p class="mt-[14rem] lg:mt-16 lg:text-[1rem]">
+        <p class="mt-[14rem] lg:mt-10 lg:text-[1rem]">
           Specializing in simplicity <br />
           and functionality
         </p>
