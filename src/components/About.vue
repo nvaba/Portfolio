afbc7f1142870dbb64cbad95fccffbca6137ff1f
Nate <nateaba@gmail.com>
2023-06-28 17:29:03 -0700

    Replace margin spacing with flex columns

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,11 +2,12 @@
   <section class="fade-in-slide-up" v-if="aboutContent.length > 0">
     <h2 id="about" class="mt-[5rem]">About</h2>
     <article>
-      <ul class="about-list mt-2 rounded-lg bg-accordion p-4 shadow-lg">
+      <ul
+        class="about-list mt-2 flex flex-col gap-4 rounded-lg bg-accordion p-4 shadow-lg"
+      >
         <li
           v-for="item in aboutContent[0].acf.about_repeater"
           :key="item.about_heading"
-          class="mb-5"
         >
           <h4 class="text-headingtext">{{ item.about_heading }}</h4>
           <p>{{ item.about_text_area }}</p>
